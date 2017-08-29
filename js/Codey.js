81248a4a923457f7ad078b4b99683ac449807dd8
Momopey <m22461@gmail.com>
2017-08-29 12:32:55 +0800

    Update Codey.js

--- js/Codey.js
+++ js/Codey.js
@@ -1,4 +1,4 @@
 var c=document.getElementById("myCanvas");
-				var ctx=c.getContext("2d");
-				ctx.rect(20,20,150,100);
-				ctx.stroke();
+var ctx=c.getContext("2d");
+ctx.rect(20,20,150,100);
+ctx.stroke();
