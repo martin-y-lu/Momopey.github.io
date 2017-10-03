d5fd6e9176f162f14a3853def0d8124955aaf066
Momopey <m22461@gmail.com>
2017-10-03 19:32:56 +0800

    Update Bounce.js

--- js/Bounce.js
+++ js/Bounce.js
@@ -60,7 +60,7 @@ function ball(Pos,Vel,Acc){
   this.Update=function(){
     this.Pos=this.NextPos();
     this.Vel.set(Vadd(this.Vel,this.Acc));
-    this.Acc.set(new vect(0,0.1));
+    this.Acc.set(new vect(0,0.4));
   }
   this.NextPos=function(){
       return Vadd(this.Pos,Vadd(this.Vel,Vmult(this.Acc,0.5)));
